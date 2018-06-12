bc63a10182167fd358f99eaa13806fa92cd8d45c
Hudamdd <36892645+Hudamdd@users.noreply.github.com>
2018-06-12 10:14:49 -0400

    rmjyj

--- main.lua
+++ main.lua
@@ -5,3 +5,24 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+-- Created by: Your Name
+-- Date: Month Day, Year
+-- Description: This calls the splash screen of the app to load itself.
+-----------------------------------------------------------------------------------------
+
+-- Hiding Status Bar
+-- HIDE THE STATUS BAR
+
+-----------------------------------------------------------------------------------------
+
+-- Use composer library
+local composer = require( "composer" )
+
+-----------------------------------------------------------------------------------------
+
+-- Go to the intro screen
+composer.gotoScene( "splash_screen" )
